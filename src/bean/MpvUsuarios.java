11a577c81e14fff791cf97e02674496af325d6c5
arthur.oliveira5@estudante.ifms.edu.br <arthur.oliveira5@estudante.ifms.edu.br>
2026-04-14 14:38:11 -0400

    criei dao.

--- src/bean/MpvUsuarios.java
+++ src/bean/MpvUsuarios.java
@@ -4,10 +4,131 @@
  */
 package bean;
 
+import java.util.Date;
 /**
  *
  * @author u06769532123
  */
 public class MpvUsuarios {
+    private int mpvIdUsuarios;
+    private String mpvNome;
+    private String mpvApelido;
+    private String mpvcpf;
+    private Date mpvDataNascimento;
+    private int mpvNivel;
+    private String mpvSenha;
+    private String mpvAtivo;
+
+    /**
+     * @return the mpvIdUsuarios
+     */
+    public int getMpvIdUsuarios() {
+        return mpvIdUsuarios;
+    }
+
+    /**
+     * @param mpvIdUsuarios the mpvIdUsuarios to set
+     */
+    public void setMpvIdUsuarios(int mpvIdUsuarios) {
+        this.mpvIdUsuarios = mpvIdUsuarios;
+    }
+
+    /**
+     * @return the mpvNome
+     */
+    public String getMpvNome() {
+        return mpvNome;
+    }
+
+    /**
+     * @param mpvNome the mpvNome to set
+     */
+    public void setMpvNome(String mpvNome) {
+        this.mpvNome = mpvNome;
+    }
+
+    /**
+     * @return the mpvApelido
+     */
+    public String getMpvApelido() {
+        return mpvApelido;
+    }
+
+    /**
+     * @param mpvApelido the mpvApelido to set
+     */
+    public void setMpvApelido(String mpvApelido) {
+        this.mpvApelido = mpvApelido;
+    }
+
+    /**
+     * @return the mpvcpf
+     */
+    public String getMpvcpf() {
+        return mpvcpf;
+    }
+
+    /**
+     * @param mpvcpf the mpvcpf to set
+     */
+    public void setMpvcpf(String mpvcpf) {
+        this.mpvcpf = mpvcpf;
+    }
+
+    /**
+     * @return the mpvDataNascimento
+     */
+    public Date getMpvDataNascimento() {
+        return mpvDataNascimento;
+    }
+
+    /**
+     * @param mpvDataNascimento the mpvDataNascimento to set
+     */
+    public void setMpvDataNascimento(Date mpvDataNascimento) {
+        this.mpvDataNascimento = mpvDataNascimento;
+    }
+
+    /**
+     * @return the mpvNivel
+     */
+    public int getMpvNivel() {
+        return mpvNivel;
+    }
+
+    /**
+     * @param mpvNivel the mpvNivel to set
+     */
+    public void setMpvNivel(int mpvNivel) {
+        this.mpvNivel = mpvNivel;
+    }
+
+    /**
+     * @return the mpvSenha
+     */
+    public String getMpvSenha() {
+        return mpvSenha;
+    }
+
+    /**
+     * @param mpvSenha the mpvSenha to set
+     */
+    public void setMpvSenha(String mpvSenha) {
+        this.mpvSenha = mpvSenha;
+    }
+
+    /**
+     * @return the mpvAtivo
+     */
+    public String getMpvAtivo() {
+        return mpvAtivo;
+    }
+
+    /**
+     * @param mpvAtivo the mpvAtivo to set
+     */
+    public void setMpvAtivo(String mpvAtivo) {
+        this.mpvAtivo = mpvAtivo;
+    }
     
 }
